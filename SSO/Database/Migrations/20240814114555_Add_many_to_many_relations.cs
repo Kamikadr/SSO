624718d7f9607ae70c0742b5002a512644febacb
Kamikadr <60061947+Kamikadr@users.noreply.github.com>
2024-08-14 19:11:31 +0700

    migrations

---
+++ SSO/Database/Migrations/20240814114555_Add_many_to_many_relations.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SSO.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_many_to_many_relations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
